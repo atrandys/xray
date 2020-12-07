3c79540b5977d9b96eb359debd13e1bce1a81331
atrandys <37266675+atrandys@users.noreply.github.com>
2020-12-07 23:42:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -130,7 +130,7 @@ check_port(){
 install_nginx(){
     green "$(date +"%Y-%m-%d %H:%M:%S") ==== 安装nginx"
     $systemPackage install -y nginx
-    if [ -f "/etc/nginx" ]; then
+    if [ ! -d "/etc/nginx" ]; then
         red "$(date +"%Y-%m-%d %H:%M:%S") - 看起来nginx没有安装成功，请先使用脚本中的删除xray功能，然后再重新安装.\n== Install failed."
         exit 1
     fi
